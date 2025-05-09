db3facc34ca77b8db39ce4a8d7bd6f7c0b59a22b
VanoTheOne <van0the0ne@gmail.com>
2025-05-09 11:22:44 +0300

    fixed allure

--- playwright.config.js
+++ playwright.config.js
@@ -25,6 +25,7 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    ['list'],
     ['allure-playwright'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -34,6 +35,7 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
     headless: true,
     video: 'retain-on-failure',
   },
