a799df6293826bedfeb5a3ef1289d86c9478cd38
VanoTheOne <van0the0ne@gmail.com>
2025-08-19 19:47:40 +0300

    uncommited tests

--- vigbo/tests/gelleries.spec.js
+++ vigbo/tests/gelleries.spec.js
@@ -29,37 +29,37 @@ test.describe('Vigbo main page tests', function () {
       await expect(await gelleries.newGelleryName).toHaveText('Новая галерея');
     });
 
-    // test('Should check if limit of photos for selection is 10 by default', async ({ page }) => {
-    //   await navigationMenu.openGelleries();
-    //   await gelleries.openFavoritesSettings();
-    //   await newGellery.turnOnLimitOfPhotosCheck();
-    //   await expect(await newGellery.maxPhotoAmountInput).toHaveValue('10');
-    // });
+    test('Should check if limit of photos for selection is 10 by default', async ({ page }) => {
+      await navigationMenu.openGelleries();
+      await gelleries.openFavoritesSettings();
+      await newGellery.turnOnLimitOfPhotosCheck();
+      await expect(await newGellery.maxPhotoAmountInput).toHaveValue('10');
+    });
 
-    // test('Should check the title of watched gellery', async ({ page }) => {
-    //   await navigationMenu.openGelleries();
-    //   const newPage = await gelleries.watchGellery();
-    //   await expect(newPage).toHaveTitle('Новая галерея');
-    // });
+    test('Should check the title of watched gellery', async ({ page }) => {
+      await navigationMenu.openGelleries();
+      const newPage = await gelleries.watchGellery();
+      await expect(newPage).toHaveTitle('Новая галерея');
+    });
 
-    // test('Should check if new gellery is deleted', async ({ page }) => {
-    //   await navigationMenu.openGelleries();
-    //   await gelleries.deleteFirstGellery();
-    //   await expect(await gelleries.gelleryInList).not.toBeVisible();
-    // });
+    test('Should check if new gellery is deleted', async ({ page }) => {
+      await navigationMenu.openGelleries();
+      await gelleries.deleteFirstGellery();
+      await expect(await gelleries.gelleryInList).not.toBeVisible();
+    });
 
-    // test('Should check if message about notifications turned on appears', async ({ page }) => {
-    //   await navigationMenu.openGelleries();
-    //   await gelleries.openReviews();
-    //   await newGellery.turnOnSendingNotifications();
-    //   await expect(await newGellery.appearingPopup).toContainText('включена');
-    // });
+    test('Should check if message about notifications turned on appears', async ({ page }) => {
+      await navigationMenu.openGelleries();
+      await gelleries.openReviews();
+      await newGellery.turnOnSendingNotifications();
+      await expect(await newGellery.appearingPopup).toContainText('включена');
+    });
 
-    // test('Should check if message about notifications turned off appears', async ({ page }) => {
-    //   await navigationMenu.openGelleries();
-    //   await gelleries.openReviews();
-    //   await newGellery.turnOnSendingNotifications();
-    //   await expect(await newGellery.appearingPopup).toContainText('выключена');
-    // });
+    test('Should check if message about notifications turned off appears', async ({ page }) => {
+      await navigationMenu.openGelleries();
+      await gelleries.openReviews();
+      await newGellery.turnOnSendingNotifications();
+      await expect(await newGellery.appearingPopup).toContainText('выключена');
+    });
   });
 });
